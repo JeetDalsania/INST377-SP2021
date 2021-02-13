599bbac9e72dcd37f1a38debc3f4ff768c0c4f1c
JeetDalsania <JeetDalsania@users.noreply.github.com>
2021-02-12 23:55:52 -0500

    lab 3 temp code

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,33 @@
 /* Put your javascript in here */
+
+let count = 3;
+const FIRST_INDEX = 0;
+let position = 0;
+let array1 = [];
+
+function rotateCarousel(){
+    const images = document.querySelectorAll('.imageClass');
+    const imageArray = Array.from(images);
+
+    array1.forEach((element) => {
+        console.log(element);
+     })
+
+    carousel.querySelectorAll('arrow left')
+    if (position > FIRST_INDEX){
+        position -= count;
+    }
+     
+    carousel.querySelectorAll('arrow right')
+    if (position <= 7 ){
+        position += count;
+    }
+}
+
+function onLoadOfPage() {
+	document.addEventListener('click', (event) => {
+	  rotateCarousel()
+  })
+}
+
+window.onload = onLoadOfPage;
