6ecac016d139259a992d030102adf6d8a66cfb0a
JeetDalsania <JeetDalsania@users.noreply.github.com>
2021-02-18 23:45:38 -0500

    lab 4 code v1

---
+++ public/labs/lab_4/script.js
@@ -0,0 +1,12 @@
+function onLoad() {
+  console.log("script loaded");
+}
+
+window.onload = onLoad;
+
+app.route("/api")
+    .post(async (req, res) => {
+        console.log('POST request detected');
+        console.log('Form data in res.body', req.body);
+        res.send("Hello Word");
+    });
